0ebebfb5682bffc3ba71ebf03b893862a331c0b0
Md. Shaiful Islam <islambuet@gmail.com>
2023-04-24 13:46:22 +0600

    Fixed generalview, added conveyor new state,changed photoeye colors

--- js/nav.js
+++ js/nav.js
@@ -406,7 +406,7 @@ ipcRenderer.on("link:changed", async function(e, ip_list_html, machine_list_from
     }
     function setPhotoeyesStates(machineId,inputsStates,inputsInfo){
         //console.log(inputsStates,inputsInfo)
-        let input_colors = {"in-active" : "#f7931e", "active" : "#39b54a"};
+        let input_colors = {"in-active" : "#39b54a", "active" : "#f7931e"};
         for(let key in inputsInfo){
             let inputInfo=inputsInfo[key];
             if((inputInfo['input_type']==0)&&(inputInfo['device_type']==0)&&(inputInfo['device_number']==0)&& (inputInfo['gui_input_id']>0)){
@@ -430,7 +430,7 @@ ipcRenderer.on("link:changed", async function(e, ip_list_html, machine_list_from
         }
     }
     function setConveyorsStates(machineId,conveyorsStates,conveyorsInfo){
-        let conveyor_colors = { "0" : "#ccc",  "1" : "#27e22b", "2" : "#ffc000", "3" : "red"};
+        let conveyor_colors = { "0" : "#ccc",  "1" : "#27e22b", "2" : "#ffc000", "3" : "red","4":"#87cefa"};
         for(let conveyor_id in conveyorsStates){
             $('.conveyor[conveyor-id='+conveyor_id+'] .status').css('fill',conveyor_colors[conveyorsStates[conveyor_id]]);
         }
